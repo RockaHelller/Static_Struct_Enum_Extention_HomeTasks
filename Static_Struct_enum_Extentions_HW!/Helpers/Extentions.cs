e025ae9833e1acce4e019c087612406e8af0111c
Murad Jafarov <muradjj@code.edu.az>
2022-10-25 12:24:06 +0400

    Added file

---
+++ Static_Struct_enum_Extentions_HW!/Helpers/Extentions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Static_Struct_enum_Extentions_HW_.Helpers
+{
+    public static class Extentions
+    {
+        public static double GetFactorial(this int a)
+        {
+            double sum = 1;
+
+            for (int i = 1; i <= a; i++)
+            {
+                sum *= i;
+            }
+            return sum;
+        }
+    }
+}
